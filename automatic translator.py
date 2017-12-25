a65dbe290f6f4f4c66efff396be60099a5a9c73b
steven14ggyy <32742691+steven14ggyy@users.noreply.github.com>
2017-12-25 10:49:40 +0800

    Add files via upload

--- automatic translator.py
+++ automatic translator.py
@@ -8,6 +8,15 @@ import wave
 import math
 import scipy.io.wavfile
 # =============================================================================
+# Additional Package
+# =============================================================================
+from playsound import playsound
+# =============================================================================
+# Pre-emphasis
+# =============================================================================
+def pre_emphasis(signal,coefficient=0.97):
+    return np.append(signal[0],signal[1:]-coefficient*signal[:-1])
+# =============================================================================
 # Record function
 # =============================================================================
 def RecordAudio(Audio_output_path):
@@ -15,7 +24,7 @@ def RecordAudio(Audio_output_path):
     FORMAT = pyaudio.paInt16
     CHANNELS = 1
     RATE = 16000
-    RECORD_SECONDS = 1
+    RECORD_SECONDS = 0.5
     Audio_output_path = Audio_output_path
     p = pyaudio.PyAudio()
     stream = p.open(format=FORMAT, channels = CHANNELS, rate = RATE,
@@ -37,12 +46,12 @@ def RecordAudio(Audio_output_path):
     wf.writeframes(b''.join(frames))
     wf.close()
     
-
-def main():
+def DetectSound(Record_File_Path):
     file_path = './record voice/detectvoice.wav'     
     fs = 16000
     detect = 0
-    threshold = 1
+    threshold = 0.5
+    
     while(detect!=1):
         RecordAudio(file_path)
         fs, detectvoice = wavfile.read('./record voice/detectvoice.wav')
@@ -53,7 +62,7 @@ def main():
             detect = 1
         else:
             detect = 0
-    print("* start recoding")    
+    #print("* start recoding")    
     
     stop = 0
     recordvoice = detectvoice
@@ -62,15 +71,13 @@ def main():
         fs, detectvoice = wavfile.read('./record voice/detectvoice.wav')
         detectvoice = detectvoice / (2.**15)
         Energy_sum = sum(detectvoice**2)
-        #print(Energy_sum)
+        print(Energy_sum)
         recordvoice = np.hstack((recordvoice,detectvoice))
         if(Energy_sum < threshold):
             stop = 1
         else:
-            stop = 0
-    e = 1
-            
-    file_path = './record voice/recordvoice.wav' 
+            stop = 0    
+    file_path = Record_File_Path 
     wave_data = np.int16(recordvoice* (2.**15))
     f = wave.open(file_path, "wb")
     f.setnchannels(1)
@@ -78,6 +85,38 @@ def main():
     f.setframerate(fs)
     f.writeframes(wave_data.tostring())
     f.close()
+def FindLanguage(Record_File_Path):
+    Compare_File_Path = './comparing voice data/'
+    Response_File_Path = './response voice data/'
+    Language_test, fs0 = lib.load(Record_File_Path)
+    Language_ch, fs1 = lib.load(Compare_File_Path + 'translate_ch.wav')
+    Language_en, fs2 = lib.load(Compare_File_Path + 'translate_en.wav')
+    Language_jp, fs3 = lib.load(Compare_File_Path + 'translate_jp.wav')
+    MFCC_test = lib.feature.mfcc(y=pre_emphasis(signal = Language_test), sr=fs0, n_mfcc=20)
+    MFCC_ch = lib.feature.mfcc(y=Language_ch, sr=fs1, n_mfcc=20)
+    MFCC_en = lib.feature.mfcc(y=Language_en, sr=fs2, n_mfcc=20)
+    MFCC_jp = lib.feature.mfcc(y=Language_jp, sr=fs3, n_mfcc=20)
+    D_ch, wp_ch = lib.dtw(MFCC_test, MFCC_ch)
+    D_en, wp_en = lib.dtw(MFCC_test, MFCC_en)
+    D_jp, wp_jp = lib.dtw(MFCC_test, MFCC_jp)
+    g = D_ch[-1,-1]
+    gg = D_en[-1,-1]
+    ggg = D_jp[-1,-1]
+    Shortest_D = min(D_ch[-1,-1], D_en[-1,-1], D_jp[-1,-1])
+    playsound(Record_File_Path)
+    if(Shortest_D==D_ch[-1,-1]):
+        playsound(Response_File_Path+'language_response_ch.wav')
+    elif(Shortest_D==D_en[-1,-1]):
+        playsound(Response_File_Path+'language_response_en.wav')
+    else:
+        playsound(Response_File_Path+'language_response_jp.wav')
+    e = 1
+def main():
+    Record_File_Path = './record voice/recordvoice.wav'
+    DetectSound(Record_File_Path)
+    FindLanguage(Record_File_Path)
+    
+    
             
 if __name__ == '__main__':
 	main()
